602fb9547901048ceed4d1ee89905b2be194ef72
Micheal <Micheal@NOIR>
2024-04-10 08:08:18 +0100

    Add project files.

---
+++ AsyncAwaitClosure/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace AsyncAwaitClosure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool closure = false;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                int captureValue = i;
+
+                ThreadPool.QueueUserWorkItem(delegate
+                {
+                    if (closure)
+                    {
+                        Console.WriteLine(captureValue);
+                    }
+                    else
+                    {
+                        Console.WriteLine(i);
+                    }
+                    Thread.Sleep(1000);
+                });
+            }
+            Console.ReadLine();
+        }
+    }
+}
